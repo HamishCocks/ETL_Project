437d37bc0e74264e03ca0754dae898561f0cc5fb
Hamish Cocks <hpcoc2@gmail.com>
2021-05-10 21:20:37 +1000

    schema change

--- schema.sql
+++ schema.sql
@@ -6,27 +6,35 @@ Director VARCHAR(300),
 Run_Time VARCHAR(300),
 Age_Rating VARCHAR(300),
 Genre VARCHAR(300),
-PRIMARY KEY (Title, ID)
+	Primary Key (Release_Date)
 );
 
 create table Rating (
 Title VARCHAR(100),
+Release_Date VARCHAR(300),
 IMDB FLOAT,
-Rotten_Tomatoes VARCHAR(30)
+Rotten_Tomatoes VARCHAR(30),
+	Primary Key (Release_Date)
 );
 
-create table Casting(
+create table Casting (
 Title VARCHAR(100),
+Release_Date VARCHAR(300),
 Hero VARCHAR(100),
 Villain VARCHAR(100),
 Character VARCHAR(100),
-Voice_Actor VARCHAR(100)
+Voice_Actor VARCHAR(100),
+	Foreign key (Release_Date) references Basic(Release_Date),
+	Foreign key (Release_Date) references Crew(Release_Date),
+	Foreign key (Release_Date) references Casting(Release_Date),
 );
 
 create table Crew (
 Title VARCHAR(300),
+Release_Date VARCHAR(300),
 Composer VARCHAR(300),
 Cinematographer VARCHAR(300),
 Editor VARCHAR(300),
-Screenplay VARCHAR(300)
+Screenplay VARCHAR(300),
+	Primary Key (Release_Date)
 );
